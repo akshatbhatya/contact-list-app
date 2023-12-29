3a6cd811cabe97959aedded64664cf881badb7a9
akshat <akshatbhatya@gmail.com>
2023-12-29 12:17:38 +0530

    add some tags in contact form and add some functionality in context

--- contact-List_App/src/Components/Contact Form/ContactForm.jsx
+++ contact-List_App/src/Components/Contact Form/ContactForm.jsx
@@ -8,6 +8,14 @@ function ContactForm() {
         <form action="">
             <label htmlFor="">Name</label>
             <input type="text" placeholder='Enter Your Name' />
+            <br />
+            <br />
+            <label htmlFor="">Email</label>
+            <input type="email" placeholder='Enter Your Email'/>
+            <br />
+            <br />
+            <label htmlFor="">Number</label>
+            <input type="text" placeholder='Enter Your Number' />
         </form>
     </div>
     </>
--- contact-List_App/src/Contexts/Context.js
+++ contact-List_App/src/Contexts/Context.js
@@ -1,9 +1,21 @@
 import { useContext, createContext } from "react";
 
-export const ContactContext = createContext({});
+export const ContactContext = createContext({
+  contacts: [
+    {
+      id: 1,
+      name: "akshat",
+      email: "akshatbhatya@gmail.com",
+      number: 7814720746,
+    },
+  ],
+  addContact: (name,email,number) => {},
+  updateContact: (id,name,email,number) => {},
+  removeContact: (id) => {},
+});
+
+export let ContactListProvider = ContactContext.Provider;
 
 export const UseContactContext = () => {
   return useContext(ContactContext);
 };
-
-export let ContactListProvider = ContactContext.Provider;
